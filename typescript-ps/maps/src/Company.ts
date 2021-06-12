dbf9101bb96fe639909fc3ac1d72d8d698ae25e3
hoon2hooni <sanghoon.engineer@gmail.com>
2021-06-12 21:04:43 +0900

    Add Company and Google Map Api Operation which not yet rendered

---
+++ typescript-ps/maps/src/Company.ts
@@ -0,0 +1,18 @@
+import faker from 'faker'
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseInt(faker.address.latitude()),
+      lng: parseInt(faker.address.longitude())
+    };
+  }
+}
