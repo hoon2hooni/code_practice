1ab35636e0e8ad41ecf27b2dcb9b6e23194de770
hoon2hooni <sanghoon.engineer@gmail.com>
2021-06-19 15:33:32 +0900

    Learn generic in function with using merge function

--- typescript-ps/generic/generics-01-starting-setup/src/app.ts
+++ typescript-ps/generic/generics-01-starting-setup/src/app.ts
@@ -1,22 +1,31 @@
 {// Code goes here!
-// const names: Array<string> = [];//
+  // const names: Array<string> = [];//
 
-//promise는 자바스크립트 feature이다
+  //promise는 자바스크립트 feature이다
 
-//promise<unknown>
-type Text = string
-const promise: Promise<Text> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("this is done");
-  }, 2000);
-});
+  //promise<unknown>
+  // type Text = string
+  // const promise: Promise<Text> = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve("this is done");
+  //   }, 2000);
+  // });
 
-//promise: Promise<number>
-//dㅣ렇게 하면 split에 오류 생김!
-promise.then(data => {
-  data.split(' ');
-})
+  // //promise: Promise<number>
+  // //dㅣ렇게 하면 split에 오류 생김!
+  // promise.then(data => {
+  //   data.split(' ');
+  // })
   
+  // }
+
+
+  function merge<T, U>(objA: T, objB: U) {
+    return {...objA, ...objB};
+  }
+  
+  console.log(merge({name:'MAx', love: ['idonno']},{age: 30}));
+
 }
 
 
